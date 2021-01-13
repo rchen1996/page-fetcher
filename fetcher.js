c79c92568b044a0f430ecb88c6a4dfea89d5d5f8
Rebecca Chen <rebecca061996@gmail.com>
2021-01-13 23:52:05 +0000

    implemented logit to allow user to overwrite the file or not

--- fetcher.js
+++ fetcher.js
@@ -13,6 +13,30 @@ const input = process.argv.slice(2);
 
 // use readline
 const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const requestServer = function() {
+  request(`${input[0]}`, (error, response, body) => {
+    // if there is no error, proceed to download file
+    if (response.statusCode >= 300) {
+      // if the given URL results in an error or non-200 result
+      // terminate the app and explain what went wrong
+      // don't write response to the file
+      console.log(`You encountered an error with status code ${response.statusCode}`)
+      process.exit();
+    } else if (!error) {
+      fs.writeFile(input[1], body, () => {
+        let stats = fs.statSync(`${input[1]}`);
+        let filesize = stats.size;
+        console.log(`Downloaded and saved ${filesize} bytes to ${input[1]}`)
+        process.exit();
+      })
+    }
+  })
+};
 
 const fetcher = function() {
   // first check if the path is valid
@@ -32,23 +56,18 @@ const fetcher = function() {
   // could also have checked with fs.exists(path, callback)
   fs.stat(pathOnly, (err) => {
     // if no error i.e. valid path
+    // check if the file already exists
+  })
+  fs.access(input[1], fs.constants.F_OK, (err) => {
+    // file exists
     if (!err) {
-      // make the request and write the file
-      request(`${input[0]}`, (error, response, body) => {
-        // if there is no error, proceed to download file
-        if (response.statusCode >= 300) {
-          // if the given URL results in an error or non-200 result
-          // terminate the app and explain what went wrong
-          // don't write response to the file
-          console.log(`You encountered an error with status code ${response.statusCode}`)
-        } else if (!error) {
-          fs.writeFile(input[1], body, () => {
-            let stats = fs.statSync(`${input[1]}`);
-            let filesize = stats.size;
-            console.log(`Downloaded and saved ${filesize} bytes to ${input[1]}`)
-          })
+      rl.question('The file already exists. If you would like to overwrite the file, type y and hit enter\n', (answer) => {
+        if (answer = "y") {
+          requestServer();
         }
       })
+    } else {
+      requestServer();
     }
   })
 };
