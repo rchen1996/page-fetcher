e446b13f3be4f8d5b547cd9211aefe51814aa9ce
Rebecca Chen <rebecca061996@gmail.com>
2021-01-13 20:45:11 +0000

    implemented logic for the app to take in a command line argument for URL and path and to download the file to the specified path

--- fetcher.js
+++ fetcher.js
@@ -9,9 +9,34 @@ const request = require('request');
 const fs = require('fs');
 
 // need to use command-line arguments
-const stdin = process.stdin;
+const input = process.argv.slice(2);
 
-// setup stdin
-stdin.setRawMode(true);
-stdin.setEncoding('utf8');
+const fetcher = function(url, path) {
+  // first check if the path is valid
+  fs.access(input[1], (err) => {
+    // if no error i.e. valid path
+    if (!err) {
+      // make the request and write the file
+      request(`${input[0]}`, (error, response, body) => {
+        // if there is no error, proceed to download file
+        if (!err) {
+          fs.writeFile(input[1], body, () => {
+            let stats = fs.statSync(`${input[1]}`);
+            let filesize = stats.size;
+            console.log(`Downloaded and saved ${filesize} bytes to ${input[1]}`)
+          })
+        } else {
+          // if the given URL results in an error or non-200 result
+          // terminate the app and explain what went wrong
+          // don't write response to the file
+          console.log(`You encountered an error with status code ${error}`)
+        }
+      })
+    } else {
+      // if path is not valid, fail the download
+      console.log("This path is not valid!");
+    }
+  })
+};
 
+fetcher();
