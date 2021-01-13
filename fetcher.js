17bedf52c26e834e1afb71889d4849d2adfb7367
Rebecca Chen <rebecca061996@gmail.com>
2021-01-13 20:08:42 +0000

    created file and added instructions for app

---
+++ fetcher.js
@@ -0,0 +1,4 @@
+// takes in URL as command-line argument and local file path
+// download the resource to the specified path
+// upon completion, print out a message saying Downloaded and saved x bytes to path
+
