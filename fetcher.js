3cf43a7e7043026c0b95825bd454db0904974a75
Rebecca Chen <rebecca061996@gmail.com>
2021-01-13 20:13:36 +0000

    added setup code to require packages/modules

--- fetcher.js
+++ fetcher.js
@@ -2,3 +2,16 @@
 // download the resource to the specified path
 // upon completion, print out a message saying Downloaded and saved x bytes to path
 
+// use request library to make HTTP request
+const request = require('request');
+
+// use Node's fs module to write file
+const fs = require('fs');
+
+// need to use command-line arguments
+const stdin = process.stdin;
+
+// setup stdin
+stdin.setRawMode(true);
+stdin.setEncoding('utf8');
+
